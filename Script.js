d6d8d1f57b7709bdc34132b217ca0b2066b0b15e
Sean Le <seanzx9@gmail.com>
2019-07-13 13:35:44 -0400

    welcome

--- Script.js
+++ Script.js
@@ -1,12 +1,27 @@
 //for typing effect
 var i = 0;
-var txt = ' Welcome to my Website!'; /* The text */
-var speed = 100; /* The speed/duration of the effect in milliseconds */
+var j = 0;
+var hello = " Hello.";
+var name = " I'm Sean.";
 
-function typing() {
-    if (i < txt.length) {
-        document.getElementById("first-parallax").innerHTML += txt.charAt(i);
+function welcomeMessage() {
+    typeHello();
+    setTimeout(typeName, 1500);
+}
+
+function typeHello() {
+    if (i < hello.length) {
+        document.getElementById("first-parallax").innerHTML += hello.charAt(i);
         i++;
-        setTimeout(typing, speed);
+        setTimeout(typeHello, 100);
     }
 }
+
+function typeName() {
+    if (j < name.length) {
+        document.getElementById("first-parallax").innerHTML += name.charAt(j);
+        j++;
+        setTimeout(typeName, 100);
+    }
+}
+
