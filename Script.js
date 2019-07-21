bae9a9e8f989e8d963002f15e7f1fc114dfacdf5
Sean Le <seanzx9@gmail.com>
2019-07-20 23:34:31 -0400

    welcome message

--- Script.js
+++ Script.js
@@ -1,83 +1,82 @@
-//hello typing effect
+//typing effect
 var i = 0;
-var j = 0;
-var hello = " Hello.";
-var name = " I'm Sean.";
+var hello = "Hello. I'm Sean.";
+var welcome = " Welcome to my website!"
+var typed = false;
+var deleted = false;
 
-//footer animation
-var e = 0;
-var emailOpac = 0.9;
-var emailFaded = false;
-var l = 0;
-var linkedinOpac = 0.9;
-var linkedinFaded = false;
-
-//hello typing effect
+//hello message typing effect
 function welcomeMessage() {
-    typeHello();
-    setTimeout(typeName, 1500);
-}
-
-function typeHello() {
-    if (i < hello.length) {
-        document.getElementById("home").innerHTML += hello.charAt(i);
-        i++;
-        setTimeout(typeHello, 100);
+    var txt = document.getElementById("welcome-txt");
+    if (typed == false) {
+        if (i < 6) {
+            txt.innerHTML += hello.charAt(i);
+            i++;
+            setTimeout(welcomeMessage, 125);
+        }
+        else if (i == 6) {
+            txt.innerHTML += hello.charAt(i);
+            i++;
+            setTimeout(welcomeMessage, 1000);
+        }
+        else if (i > 6 && i < hello.length) {
+            txt.innerHTML += hello.charAt(i);
+            i++;
+            setTimeout(welcomeMessage, 125);
+        }
+        else if (i == hello.length) {
+            typed = true;
+        }
     }
 }
 
-function typeName() {
-    if (j < name.length) {
-        document.getElementById("home").innerHTML += name.charAt(j);
-        j++;
-        setTimeout(typeName, 100);
+//deletes hello and retypes new welcome message
+function reWrite() {
+    var txt = document.getElementById("welcome-txt");
+
+    if (typed == true) {
+        if (i > 0 && deleted == false) {
+            i--;
+            txt.innerHTML = hello.substring(0, i);
+            setTimeout(reWrite, 150);
+        }
+        else if (i == 0) {
+            i++;
+            deleted = true;
+            setTimeout(reWrite, 500);
+
+        }
+        else if (i < welcome.length && deleted == true) {
+            txt.innerHTML += welcome.charAt(i);
+            i++;
+            setTimeout(reWrite, 125);
+        }
     }
 }
 
-//footer animation
+//waits for page to be loaded and then adds listeners
 window.onload = function () {
+    var txt = document.getElementById("welcome-txt");
     var email = document.getElementById("email-icon");
     var linkedin = document.getElementById("linkedin-icon");
 
-    //fades linkedin logo when mouse on email logo
-    email.addEventListener("mouseover", function linFade(){
-        if (linkedinFaded == false) {
-            linkedin.style.opacity = linkedinOpac;
-            linkedinOpac -= 0.01;
-            l++;
-            setTimeout(linFade, 1);
-        }
-        if (l == 50) {
-            linkedinFaded = true;
-        }
+    //for mouse going over the welcome message or any click in the screen
+    txt.addEventListener("mouseover", reWrite);
+    document.addEventListener("click", reWrite);
+
+    //for hovering over email icon
+    email.addEventListener("mouseover", function() {
+        email.style.filter = "invert(25%)";
     });
-    //makes linkedin logo solid when mouse out
-    email.addEventListener("mouseout", function linIn() {
-        linkedinFaded = false;
-        l = 0;
-        linkedinOpac = 0.9;
-        linkedin.style.opacity = linkedinOpac;
+    email.addEventListener("mouseout", function() {
+        email.style.filter = "invert(0%)";
     });
-    //makes email logo fade when mouse on linkedin logo
-    linkedin.addEventListener("mouseover", function emailOut(){
-        if (emailFaded == false) {
-            email.style.opacity = emailOpac;
-            emailOpac -= 0.01;
-            e++;
-            setTimeout(emailOut, 5);
-        }
-        if (e == 50) {
-            emailFaded = true;
-        }
+
+    //for hovering over linkedin icon
+    linkedin.addEventListener("mouseover", function() {
+        linkedin.style.filter = "invert(25%)";
     });
-    //makes email logo solid when mouse out
-    linkedin.addEventListener("mouseout", function emailIn(){
-        emailFaded = false;
-        e = 0;
-        emailOpac = 0.9;
-        email.style.opacity = emailOpac;
+    linkedin.addEventListener("mouseout", function() {
+        linkedin.style.filter = "invert(0%)";
     });
 }
-
-
-
