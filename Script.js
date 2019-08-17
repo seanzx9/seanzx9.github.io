f5398d2ced0ab4d5c5fde98a33022f9ceabadf79
Sean Le <seanzx9@gmail.com>
2019-08-16 21:24:42 -0400

    big update

--- Script.js
+++ Script.js
@@ -1,63 +1,155 @@
-//typing effect
-var i = 0;
-var hello = "Hello. I'm Sean.";
-var welcome = " Welcome to my website!"
-var typed = false;
-var deleted = false;
-
-//hello message typing effect
-function welcomeMessage() {
+//typing effect, moving arrow, fading with extending lines
+window.onload = function () {
+    var body = document.getElementsByTagName("BODY")[0];
+    //for typing welcome message
+    var i = 0;
+    var hello = "Hello! I'm Sean.";
+    var typed = false;
     var txt = document.getElementById("welcome-txt");
-    if (typed == false) {
-        if (i < 6) {
-            txt.innerHTML += hello.charAt(i);
-            i++;
-            setTimeout(welcomeMessage, 125);
-        }
-        else if (i == 6) {
-            txt.innerHTML += hello.charAt(i);
-            i++;
-            setTimeout(welcomeMessage, 1000);
-        }
-        else if (i > 6 && i < hello.length) {
-            txt.innerHTML += hello.charAt(i);
-            i++;
-            setTimeout(welcomeMessage, 125);
-        }
-        else if (i == hello.length) {
-            typed = true;
+    //for the arrow
+    var arrowShow = 0;
+    //line elements and widths
+    var line1 = document.getElementById("line1");
+    var line2 = document.getElementById("line2");
+    var line1w = line1.clientWidth;
+    var line2w = line2.clientWidth;
+
+    //invoke typing 
+    welcomeMessage();
+
+    //types out message, sets typed to true when completed
+    function welcomeMessage () {
+        if (typed === false) {
+            disableScrolling();
+            if (i < 6) {
+                txt.innerHTML += hello.charAt(i);
+                i++;
+                setTimeout(welcomeMessage, 100);
+            }
+            else if (i === 6) {
+                txt.innerHTML += hello.charAt(i);
+                i++;
+                txt.innerHTML += "<br />";
+                setTimeout(welcomeMessage, 600);
+            }
+            else if (i > 6 && i < hello.length) {
+                txt.innerHTML += hello.charAt(i);
+                i++;
+                setTimeout(welcomeMessage, 100);
+            }
+            else if (i === hello.length) {
+                typed = true;
+                enableScrolling();
+            }
         }
     }
 
-    //for mouse going over the welcome message or any click in the screen
-    txt.addEventListener("mouseover", reWrite);
-    document.addEventListener("click", reWrite);
-}
-
-//deletes hello and retypes new welcome message
-function reWrite() {
-    var txt = document.getElementById("welcome-txt");
-
-    //removes listeners
-    txt.removeEventListener("mouseover", reWrite);
-    document.removeEventListener("click", reWrite);
-
-    if (typed == true) {
-        if (i > 0 && deleted == false) {
-            i--;
-            txt.innerHTML = hello.substring(0, i);
-            setTimeout(reWrite, 150);
-        }
-        else if (i == 0) {
-            i++;
-            deleted = true;
-            setTimeout(reWrite, 500);
-
-        }
-        else if (i < welcome.length && deleted == true) {
-            txt.innerHTML += welcome.charAt(i);
-            i++;
-            setTimeout(reWrite, 125);
-        }
+    function disableScrolling() {
+        body.style.overflow = "hidden";
     }
+
+    function enableScrolling () {
+        body.style.overflow = "hidden";
+    }
+
+    //shows moving arrow
+    setInterval(function () {
+        if (typed === true) {
+            //hide element if scroll too far down
+            if (window.pageYOffset > 500) {
+                document.getElementById("arrow").style.visibility = "hidden";
+            }
+            if (arrowShow === 0) {
+                document.getElementById("arrow").style.visibility = "visible";
+                document.getElementById("arrow").style.bottom = "-20px";
+                arrowShow = 2;
+            }
+            else if (arrowShow === 2) {
+                document.getElementById("arrow").style.bottom = "-50px";
+                arrowShow = 1;
+            }
+            else {
+                document.getElementById("arrow").style.bottom = "-80px";
+                arrowShow = 0;
+            }
+        }
+    }, 400);
+
+    //follows scroll to delete characters or change lines
+    setInterval(function () {
+        if (typed === true) {
+            //don't extend if past page width
+            if (line2w + (5 * window.pageYOffset) < document.getElementById("home").scrollWidth - 20) {
+                line1.style.width = line1w + (5 * window.pageYOffset) + "px";
+                line2.style.width = line2w + (5 * window.pageYOffset) + "px";
+            }
+
+            if (window.pageYOffset > 290) {
+                txt.innerHTML = "";
+                line1.style.opacity = "0.05";
+                line2.style.opacity = "0.05";
+            }
+            else if (window.pageYOffset >= 280) {
+                txt.innerHTML = "<br />" + hello.substring(12, 13);
+                line1.style.opacity = "0.1";
+                line2.style.opacity = "0.1";
+            }
+            else if (window.pageYOffset >= 270) {
+                txt.innerHTML = "<br />" + hello.substring(11, 13);
+                line1.style.opacity = "0.15";
+                line2.style.opacity = "0.15";
+            }
+            else if (window.pageYOffset >= 250) {
+                txt.innerHTML = "<br />" + hello.substring(9, 9) + hello.substring(10, 13);
+                line1.style.opacity = "0.2";
+                line2.style.opacity = "0.2";
+            }
+            else if (window.pageYOffset >= 220) {
+                txt.innerHTML = hello.substring(3, 4) + "<br />" + hello.substring(8, 9) + hello.substring(10, 13);
+                line1.style.opacity = "0.25";
+                line2.style.opacity = "0.25";
+            }
+            else if (window.pageYOffset >= 190) {
+                txt.innerHTML = hello.substring(3, 4) + "<br />" + hello.substring(7, 9) + hello.substring(10, 13);
+                line1.style.opacity = "0.3";
+                line2.style.opacity = "0.3";
+            }
+            else if (window.pageYOffset >= 160) {
+                txt.innerHTML = hello.substring(3, 5) + "<br />" + hello.substring(7, 9) + hello.substring(10, 13) + hello.substring(15, 15);
+                line1.style.opacity = "0.4";
+                line2.style.opacity = "0.4";
+            }
+            else if (window.pageYOffset >= 130) {
+                txt.innerHTML = hello.substring(2, 5) + "<br />" + hello.substring(7, 9) + hello.substring(10, 13) + hello.substring(15, 15);
+                line1.style.opacity = "0.5";
+                line2.style.opacity = "0.5";
+            }
+            else if (window.pageYOffset >= 100) {
+                txt.innerHTML = hello.substring(2, 6) + "<br />" + hello.substring(7, 9) + hello.substring(10, 13) + hello.substring(15, 16);
+                line1.style.opacity = "0.6";
+                line2.style.opacity = "0.6";
+            }
+            else if (window.pageYOffset >= 70) {
+                txt.innerHTML = hello.substring(2, 6) + "<br />" + hello.substring(7, 9) + hello.substring(10, 14) + hello.substring(15, 16);
+                line1.style.opacity = "0.7";
+                line2.style.opacity = "0.7";
+            }
+            else if (window.pageYOffset >= 40) {
+                txt.innerHTML = hello.substring(2, 6) + "<br />" + hello.substring(7, 9) + hello.substring(10, 16);
+                line1.style.opacity = "0.8";
+                line2.style.opacity = "0.8";
+
+            }
+            else if (window.pageYOffset >= 10) {
+                txt.innerHTML = hello.substring(1, 6) + "<br />" + hello.substring(7, 16);
+                line1.style.opacity = "0.9";
+                line2.style.opacity = "0.9";
+            }
+            else {
+                txt.innerHTML = hello.substring(0, 6) + "<br />" + hello.substring(7, 16);
+                line1.style.opacity = "1";
+                line2.style.opacity = "1";
+            }
+        }
+    }, 100);
 }
