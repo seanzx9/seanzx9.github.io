73c80f461e4d367dce6b823c752697f25258c38a
seanzx9 <seanzx9@gmail.com>
2020-09-28 15:55:08 -0400

    Edited homepage animation

--- Script.js
+++ Script.js
@@ -155,11 +155,11 @@ window.onload = function () {
 
         if (typed === true) {
             if (window.innerWidth > 768) {
-                if (line1w > 0 && line1w < window.innerWidth)
-                    line1.style.width = line1w + (window.pageYOffset) + "px";
+                if (line1w > 0)
+                    line1.style.width = line1w - (window.pageYOffset) + "px";
 
-                if (line2w > 0 && line2w < window.innerWidth)
-                    line2.style.width = line2w + (window.pageYOffset) + "px";
+                if (line2w > 0)
+                    line2.style.width = line2w - (window.pageYOffset) + "px";
             }
             else {
                 if (line1w > 0)
